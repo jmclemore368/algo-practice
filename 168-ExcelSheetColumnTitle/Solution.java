b3075551dce36cb215eaaa5e65cc9f9895dd0963
Jeffrey McLemore <jeffreymclemore@jeffreys-mbp.lan>
2018-10-14 23:28:43 -0400

    lc168

---
+++ 168-ExcelSheetColumnTitle/Solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String convertToTitle(int n) {
+      // Take off the last number by subtracting by its value
+      // Then "remove" a column by divide by 26
+        StringBuilder sb = new StringBuilder();
+        while (n > 0) {
+            // Subtract 1 so A maps to 0 in Mod and Z maps to 25
+            int lastNum = (n - 1) % 26;
+            char lastAscii = (char) (65 + last);
+            sb.append(lastAscii);
+            n = (n - last) / 26;
+        }
+        return sb.reverse().toString();
+    }
+}
