6740f873198be2b8db8e5ce6d577268d4736934f
Jeffrey McLemore <jeffreymclemore@jeffreys-mbp.lan>
2018-11-03 03:22:39 -0400

    lc88-update

--- LeetCode/Easy/88-MergeSortedArray/Solution.java
+++ LeetCode/Easy/88-MergeSortedArray/Solution.java
@@ -1,24 +1,21 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = nums1.length - 1;
         // Fill from the back
-        while (m > 0 || n > 0) {
-            if (m == 0) {
-                nums1[i] = nums2[n - 1];
-                n--;
-            } else if (n == 0) {
+        int i = nums1.length - 1;
+        while (m > 0 && n > 0) {
+            if (nums1[m - 1] >= nums2[n - 1]) {
                 nums1[i] = nums1[m - 1];
-                m--;
+                --m;
             } else {
-                if (nums1[m - 1] >= nums2[n - 1]) {
-                    nums1[i] = nums1[m - 1];
-                    m--;
-                } else {
-                    nums1[i] = nums2[n - 1];
-                    n--;
-                }
+                nums1[i] = nums2[n - 1];
+                --n;
             }
-            i--;
+            --i;
+        }
+        while (n > 0) {
+            nums1[i] = nums2[n - 1];
+            --n;
+            --i;
         }
     }
 }
